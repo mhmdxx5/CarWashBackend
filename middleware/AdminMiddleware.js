f2b56114ad4568167155e20ed3ff907bdcc011be
Muhammed Otmani <muhammedotmani@Muhammeds-Laptop.local>
2025-05-24 19:21:07 +0300

    mtoM

--- middleware/AdminMiddleware.js
+++ middleware/AdminMiddleware.js
@@ -1,6 +1,6 @@
 const AdminMiddleware = (req, res, next) => {
     // التأكد إن المستخدم مسجل وله صلاحية "Admin"
-    if (req.user && req.user.role === "Admin") {
+    if (req.user && req.user.role === "admin") {
       next();
     } else {
       return res.status(403).json({ message: "⛔ ليس لديك صلاحية كمشرف" });
