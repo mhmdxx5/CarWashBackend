b2e020efaa1c59146e205a2f00ab4dcefde22831
Muhammed Otmani <muhammedotmani@Muhammeds-Laptop.local>
2025-06-05 15:37:21 +0300

    middle

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -47,7 +47,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "180d" }
     );
 
     res.json({
